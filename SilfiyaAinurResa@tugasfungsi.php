30b639cf49a91b4adceb6e27ae08b57d5c881fb3
SilfiyaAinurResa <109323543+SilfiyaAinurResa@users.noreply.github.com>
2022-08-23 10:28:37 +0700

    Add files via upload

---
+++ SilfiyaAinurResa@tugasfungsi.php
@@ -0,0 +1,14 @@
+<?php
+$panjang=15;
+$lebar=12;
+$tinggi=8;
+$volume=$panjang*$lebar*$tinggi;
+echo "Hasil Volume Balok dengan p = 15 cm, l = 12 cm, dan t = 8 cm adalah $volume cm3";
+
+echo '</br>';
+
+$r=14;
+$t=15;
+$volume=1/3*M_PI*$r*$r*$t;
+echo "Hasil Volume Kerucut dengan t = 15 cm dan r = 14 cm adalah $volume cm3";
+?>
